8234cd8ad6913c4b2a9ac8dd45c4bfc69c59b80b
Gabriel Ross <gabriel.ross@outlook.com>
2021-08-23 20:06:42 -0700

    created maven project

---
+++ src/main/java/projects/gabeross/springrecipemanager/SpringRecipeManagerApplication.java
@@ -0,0 +1,13 @@
+package projects.gabeross.springrecipemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRecipeManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRecipeManagerApplication.class, args);
+    }
+
+}
